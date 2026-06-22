9bab2b8cd8f71779bae93a9fcd86a8450c5e201f
RijansPatoliya <rijans.patoliya.cg@gmail.com>
2026-06-22 15:38:34 +0530

    feat: update user schema to ensure unique username and lowercase email

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,3 +1,4 @@
+// src/models/User.js
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
@@ -5,14 +6,16 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: [true, 'Username is required'],
+      unique: true,
       trim: true,
+      lowercase: true,
     },
     email: {
       type: String,
       required: [true, 'Email is required'],
       unique: true,
-      lowercase: true,
       trim: true,
+      lowercase: true,
     },
     password: {
       type: String,
